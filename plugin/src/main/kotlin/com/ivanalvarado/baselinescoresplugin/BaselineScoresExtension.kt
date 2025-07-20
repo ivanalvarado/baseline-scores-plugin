11d7e8c43127bee23de8898f915e72239d40cb6b
Ivan Alvarado <ivalvara21@gmail.com>
2025-07-20 11:36:15 -0700

    Add detekt Naming Rule Set (#10)
    * Add detekt Naming Rule Set
    
    * Add link

--- plugin/src/main/kotlin/com/ivanalvarado/baselinescoresplugin/BaselineScoresExtension.kt
+++ plugin/src/main/kotlin/com/ivanalvarado/baselinescoresplugin/BaselineScoresExtension.kt
@@ -229,6 +229,33 @@ open class BaselineScoresExtension {
     )
 
     /**
+     * https://detekt.dev/docs/rules/naming
+     */
+    private val namingRuleSet = mapOf(
+        "BooleanPropertyNaming" to -5,
+        "ClassNaming" to -5,
+        "ConstructorParameterNaming" to -5,
+        "EnumNaming" to -5,
+        "ForbiddenClassName" to -5,
+        "FunctionMaxLength" to -5,
+        "FunctionMinLength" to -5,
+        "FunctionNaming" to -5,
+        "FunctionParameterNaming" to -5,
+        "InvalidPackageDeclaration" to -5,
+        "LambdaParameterNaming" to -5,
+        "MatchingDeclarationName" to -5,
+        "MemberNameEqualsClassName" to -5,
+        "NoNameShadowing" to -5,
+        "NonBooleanPropertyPrefixedWithIs" to -5,
+        "ObjectPropertyNaming" to -5,
+        "PackageNaming" to -5,
+        "TopLevelPropertyNaming" to -5,
+        "VariableMaxLength" to -5,
+        "VariableMinLength" to -5,
+        "VariableNaming" to -5,
+    )
+
+    /**
      * Default scoring rules for common detekt issues
      */
     private fun getDefaultScoringRules(): Map<String, Int> {
@@ -258,6 +285,7 @@ open class BaselineScoresExtension {
                 emptyBlocksRuleSet +
                 exceptionsRuleSet +
                 formattingRuleSet +
-                librariesRuleSet
+                librariesRuleSet +
+                namingRuleSet
     }
 }
